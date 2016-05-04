e9dc42ae2becb7670c8a45b1a50f51a77ed88fa2
Brian Dukes <bdukes@engagesoftware.com>
2016-05-04 09:53:22 -0500

    do not error when _InstallPackages directory does not exist

--- ZipAllLibraries.ps1
+++ ZipAllLibraries.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module .\JsLibraryPackaging.psm1
-rmdir .\_InstallPackages -Recurse -Force
+rmdir .\_InstallPackages -Recurse -Force -ErrorAction SilentlyContinue
 mkdir .\_InstallPackages | Out-Null
 New-Package -Recurse
